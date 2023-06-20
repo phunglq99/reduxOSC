cc55da157283973bf286dfbaa7b01d468401f21d
phunglq99 <phunglai1999@gmail.com>
2023-06-20 10:43:07 +0700

    change

--- pj_1/game/src/components/SideBar/index.jsx
+++ pj_1/game/src/components/SideBar/index.jsx
@@ -6,11 +6,12 @@ import Group3 from '../../assets/images/Group3.png';
 import Group_Logo from '../../assets/images/Group_Logo.png';
 import Icon_Add from '../../assets/images/Icon_Add.png';
 import Frame_7 from '../../assets/images/Frame_7.png';
+import Profile from '../../assets/images/Profile.png';
 
 const SideBar = () => {
     return (
         <div className="sidebar vh-100">
-            <div className="row side-header">
+            <div className="row side-left">
                 <div className="col-3 side-menu_logo">
                     <a href="#"><img src={Logo} alt="" /></a>
                     <ul className="side-logo_menu">
@@ -43,7 +44,7 @@ const SideBar = () => {
                             <a href="#">Music</a>
                         </li>
                         <li className="side-menu_item">
-                            <i className="bi bi-playstation"></i>
+                            <i class="bi bi-controller"></i>
                             <a href="#">Gaming</a>
                         </li>
                         <li className="side-menu_item">
@@ -63,12 +64,31 @@ const SideBar = () => {
                             <a href="#">Student Hubs</a>
                         </li>
                     </ul>
+                    <div className="side-music">
+                        <img src={Frame_7} alt="" />
+                    </div>
+                    <div className="side-profile d-flex align-items-center justify-content-between px-2">
+                        <div className="d-flex align-items-center gap-2">
+                            <img src={Profile} alt="" />
+                            <h5 className="text-light fw-normal">sophiefortune</h5>
+                        </div>
+                        <div className="side-profile_mic d-flex align-items-center gap-3">
+                            <div className="test">
+                                <div className="customize-pentagon">
+                                    <div className="customize-pentagon_outline"></div>
+                                    <i class="bi bi-mic-fill"></i>
+                                </div>
+                            </div>
+                            {/* <i class="bi bi-mic-mute-fill"></i> */}
+                            <i class="bi bi-person-add"></i>
+                            <i class="bi bi-volume-up"></i>
+                        </div>
+                        
+
+                    </div>
                 </div>
             </div>
 
-            <div className="side-music">
-                <img src={Frame_7} alt="" />
-            </div>
         </div>
     )
 }
